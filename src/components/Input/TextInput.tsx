cf7eb950fa65b116a5a30a1c90bb1400bf185c5c
cbryanalfonso <cbryanalfonso@gmail.com>
2022-05-13 15:04:53 -0500

    add TextInput Component

---
+++ src/components/Input/TextInput.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {TextInput, View} from 'react-native';
+import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
+import { TextUi } from '../Text/TextUi';
+
+interface Props {
+  placeholder: string;
+  nameRightIcon?: string;
+  nameLeftIcon?: boolean;
+  autocapitalize?: boolean;
+  onChangeText: any;
+  onBlur?: any;
+  value: any;
+  style?: string;
+  security?: boolean;
+  addStyle?: any;
+}
+
+const TextInputUI = ({
+  placeholder,
+  autocapitalize,
+  onChangeText,
+  onBlur,
+  value,
+  security,
+  addStyle,
+}: Props) => {
+  return (
+    <View style={{flexDirection: 'column', marginVertical: wp(1),}}>
+    <TextUi
+         color="txtNormal"
+         texto={placeholder}
+         addStyle={{marginTop: wp(3), color:'#363636', fontSize: wp(3.5), marginBottom: wp(0.1)}}
+    />
+      <TextInput
+        placeholder={placeholder}
+        placeholderTextColor={'#BDBDBD'}
+        onChangeText={onChangeText}
+        onBlur={onBlur}
+        value={value}
+        style={[
+          {
+            borderBottomWidth: wp(0.1),
+            borderColor:'#BDBDBD'
+          },
+          addStyle ? addStyle : null,
+        ]}
+        secureTextEntry={security ? true : false}
+        autoCapitalize={autocapitalize ? 'none' : 'words'}
+      />
+    </View>
+  );
+};
+
+export default TextInputUI;
