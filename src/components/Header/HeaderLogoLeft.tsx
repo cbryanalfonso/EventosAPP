8e4dcc2c3089e8ca6e14955bff3dcd7dd31ebe07
cbryanalfonso <cbryanalfonso@gmail.com>
2022-05-17 16:03:26 -0500

    add HeaderLogo

---
+++ src/components/Header/HeaderLogoLeft.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Image, Platform, StyleSheet, View} from 'react-native';
+import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
+
+export const HeaderLogoLeft = () => {
+  return (
+    <View style={styles.safeareaTop} >
+      <Image
+        source={require('../../assets/img/Frame.png')}
+        style={styles.imageLogo}
+        resizeMode='contain'
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  imageLogo: {
+    backgroundColor: 'transparent',
+    width: wp(33),
+    height: 40,
+    //marginBottom: Platform.OS === 'ios' ?  wp(2) : wp(0),
+    //marginLeft: 0,
+  },
+  safeareaTop:{
+    backgroundColor: 'transparent',
+    //backgroundColor: 'blue',
+    width: Platform.OS === 'ios' ? "100%" : "100%",
+    height: '100%',
+    //flexDirection: "row",
+    justifyContent:"center",
+    alignItems: 'flex-start',
+    marginRight: 0, 
+     marginLeft: wp(-5),
+},
+});
