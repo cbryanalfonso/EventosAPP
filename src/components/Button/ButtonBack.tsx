26edf7b9c32a41325b7ef1fed111859cf97ced16
cbryanalfonso <cbryanalfonso@gmail.com>
2022-05-19 14:19:48 -0500

    change Icon Import

--- src/components/Button/ButtonBack.tsx
+++ src/components/Button/ButtonBack.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import {TouchableOpacity} from 'react-native';
-import {Icon} from '@rneui/themed';
+//import {Icon} from '@rneui/themed';
 import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import  Icon  from 'react-native-vector-icons/FontAwesome';
 
 export const ButtonBack = () => {
   return (
     <TouchableOpacity>
       <Icon
-        raised
+        //raised
         name="chevron-left"
-        type="font-awesome"
         color="#4F4F4F"
         size={wp(4.3)}
         onPress={() => console.log('hello')}
