68f61de282bcf7a9645b80b10b7210a33eb86421
cbryanalfonso <cbryanalfonso@gmail.com>
2022-05-19 15:22:49 -0500

    modify icon size

--- src/components/Button/ButtonNotificationHeader.tsx
+++ src/components/Button/ButtonNotificationHeader.tsx
@@ -1,17 +1,22 @@
-
-import  Icon  from 'react-native-vector-icons/FontAwesome';
-import React from 'react'
-import { TouchableOpacity } from 'react-native'
-import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import React from 'react';
+import {StyleSheet, TouchableOpacity} from 'react-native';
+import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
 
 export const ButtonNotificationHeader = () => {
   return (
-   <TouchableOpacity style={{ width: wp(11), alignItems: 'center',  height: 40, justifyContent: 'center', borderRadius: 20  }}>
-      <Icon
-        //raised
-        name='bell'
-        color='#363636'
-         />
-   </TouchableOpacity>
-  )
-}
+    <TouchableOpacity style={styles.container}>
+      <Icon name="bell" color="#363636" size={wp(5)} />
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: wp(11),
+    alignItems: 'center',
+    height: 40,
+    justifyContent: 'center',
+    borderRadius: 20,
+  },
+});
