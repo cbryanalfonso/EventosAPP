c488472e01324ec7bb706f2f56c6e9bb5b8ab59a
cbryanalfonso <cbryanalfonso@gmail.com>
2022-05-21 16:01:01 -0500

    add Principal screen

---
+++ src/Screens/HomeNewEvents/HomeNewEvents.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const HomeNewEvents = () => {
+  return (
+      <View>
+          <Text>Home Events new ...</Text>
+      </View>
+  )
+}
---
+++ src/Screens/Profile/HomeProfile.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const HomeProfile = () => {
+  return (
+   <View>
+       <Text>Home Profile ...</Text>
+   </View>
+  )
+}
