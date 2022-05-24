4f9e7f8740c7ef97a8484f7b4d26dc3524104a45
cbryanalfonso <cbryanalfonso@gmail.com>
2022-05-24 18:05:15 -0500

    modify styles in photo

--- src/components/Images/ProfileImage.tsx
+++ src/components/Images/ProfileImage.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import { Image } from 'react-native'
+import { Image, View } from 'react-native'
 import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
 
 interface Props{
     image: string,
-    width?: string,
-    height?: string,
+    width?: number,
+    height?: number,
 }
 
 export const ProfileImage = ({image, width, height}: Props) => {
   return (
-    <>
+    <View style={{backgroundColor: 'white', borderRadius: wp(5)}}>
         <Image
             source={{uri:image}}
-            style={{width: width ? width : wp(80), height: height ? height : wp(50)}}
+            style={{width: width ? wp(width) : wp(92), height: height ? wp(height) : wp(55), borderRadius: wp(5)}}
             resizeMode={'contain'}
         />
-    </>
+    </View>
   )
 }
