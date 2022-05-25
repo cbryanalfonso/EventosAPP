eb3f934537fa70a2dc373547b4bc7b0962e598e4
cbryanalfonso <cbryanalfonso@gmail.com>
2022-05-24 21:20:44 -0500

    Modify styles

--- src/components/Header/HeaderLogoLeft.tsx
+++ src/components/Header/HeaderLogoLeft.tsx
@@ -17,20 +17,18 @@ export const HeaderLogoLeft = () => {
 const styles = StyleSheet.create({
   imageLogo: {
     backgroundColor: 'transparent',
-    width: wp(33),
+    width: wp(11),
     height: 40,
     //marginBottom: Platform.OS === 'ios' ?  wp(2) : wp(0),
-    //marginLeft: 0,
   },
   safeareaTop:{
     backgroundColor: 'transparent',
     //backgroundColor: 'blue',
-    width: Platform.OS === 'ios' ? "100%" : "100%",
-    height: '100%',
+   // width: Platform.OS === 'ios' ? "100%" : "100%",
+    //height: '100%',
     //flexDirection: "row",
-    justifyContent:"center",
-    alignItems: 'flex-start',
-    marginRight: 0, 
-     marginLeft: wp(-5),
+    alignItems: 'flex-end',
+    //marginRight: 0, 
+    //marginLeft: wp(-5),
 },
 });
