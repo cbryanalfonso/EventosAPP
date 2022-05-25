57f81538d08d4cf6d8069a4fb0187c7db3a08c61
cbryanalfonso <cbryanalfonso@gmail.com>
2022-05-24 21:20:35 -0500

    modify style and add navigation

--- src/components/Button/ButtonNotificationHeader.tsx
+++ src/components/Button/ButtonNotificationHeader.tsx
@@ -2,10 +2,17 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import React from 'react';
 import {StyleSheet, TouchableOpacity} from 'react-native';
 import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import { RootStackParamListAuth } from '../../Hooks/Firebase/exportNavigations';
 
+
+type navigationHeader = NativeStackNavigationProp<RootStackParamListAuth>
 export const ButtonNotificationHeader = () => {
+
+  const navigation = useNavigation<navigationHeader>()
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={()=> navigation.navigate('Notifications') } >
       <Icon name="bell" color="#363636" size={wp(5)} />
     </TouchableOpacity>
   );
