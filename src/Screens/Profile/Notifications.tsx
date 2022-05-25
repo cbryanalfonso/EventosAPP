15bfbb2fa314ec195e9c3616a05a830a8ed541ce
cbryanalfonso <cbryanalfonso@gmail.com>
2022-05-24 21:22:56 -0500

    add Notification Screen

---
+++ src/Screens/Profile/Notifications.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+
+export const Notifications = () => {
+  return (
+      <SafeAreaView style={{backgroundColor: 'red', flex: 1,}}>
+        <Text>ksdhfkjdshfd</Text>
+      </SafeAreaView>
+  )
+}
